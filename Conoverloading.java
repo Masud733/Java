5e68127c7ab50515590364dc1263ec0a6c9b98db
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-10-31 11:57:43 +0600

    Conoverloading.java
    Constructor overloading

--- Conoverloading.java
+++ Conoverloading.java
@@ -30,3 +30,16 @@ public class Conoverloading {
         System.out.println("Hello World!");
     }
 }
+//Test class
+public class ConoverClass {
+    public static void main(String[] args){
+        Conoverloading con1 = new Conoverloading("Masud Mehrab", "Male", 22);
+        Conoverloading con2 = new Conoverloading("Azizul Haque Taushif", "Male", 25, 70);
+        Conoverloading con3 = new Conoverloading();
+        
+        System.out.println("\nName: "+con1.name+"\nAge: "+con1.age+"\nGender: "+con1.gender);
+        System.out.println("\nName: "+con2.name+"\nAge: "+con2.age+"\nGender: "+con2.gender+"\nWeight: "+con2.weight);
+    }
+        
+    
+}
