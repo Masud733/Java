fbfb68415efd43cae28b0ecbc8978bcf21774a4c
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-11-17 00:41:33 +0600

    Add files via upload
    Default constructor in java

---
+++ Dfconstructor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dfconstructor;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Dfconstructor {
+    String name, gender;
+    int age;
+
+    Dfconstructor(){
+        System.out.println("Default Constructor");
+    }
+}
