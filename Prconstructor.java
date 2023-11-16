72cb6d733f3cbc16da7f7d7a613cf90113b20443
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-11-17 02:05:38 +0600

    Add files via upload

---
+++ Prconstructor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prconstructor;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Prconstructor {
+    String name, gender;
+    int age;
+    
+    Prconstructor(String n, String s, int a){
+        name = n;
+        gender = s;
+        age = a;
+    }
+        
+
+}
