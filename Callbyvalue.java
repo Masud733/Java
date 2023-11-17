8b7ddc4184e790c6fb6fcbf259320f3fbad52aba
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-11-17 20:19:32 +0600

    Callbyvalue.java

--- Callbyvalue.java
+++ Callbyvalue.java
@@ -14,3 +14,15 @@ public class Callbyvalue {
         i=20; 
     }
 }
+public class Classcallbyvalue {
+    public static void main(String[] args){
+        Callbyvalue vl = new Callbyvalue();
+        int x= 10;
+        System.out.println("Befor call= "+x);
+        vl.change(x);
+        System.out.println("After call= "+x);
+        
+    }
+    
+}
+
