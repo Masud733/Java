30db25f34a60d3685fab60d663d401bbeedf50a5
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-11-17 12:30:58 +0600

    Add files via upload

---
+++ Staticmethod.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.staticmethod;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Staticmethod {
+    String name;
+    int id;
+    static String University = "North South";
+Staticmethod(String n, int i){
+    name = n;
+    id = i;
+}
+void display(){
+    System.out.println("Non static method.");
+    System.out.println("\nName="+name+"\nId="+id);
+    //display();//static method
+    System.out.println("University_Name="+University);
+}static void display1(){
+    System.out.println("Static method1");
+            //display2()//static method
+}
+static void display2(){
+    System.out.println("Static method2");
+    University ="TU Delft";
+    
+}
+}
