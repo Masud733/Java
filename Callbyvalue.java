9282108f79742647f4259bb4322c90b54d719f30
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-11-17 20:18:50 +0600

    Add files via upload

---
+++ Callbyvalue.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.callbyvalue;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Callbyvalue {
+
+    void change(int i){
+        i=20; 
+    }
+}
