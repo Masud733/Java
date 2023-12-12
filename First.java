f932716529c9b9ba7244a8013d1d910d8bd7e706
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-12-12 21:34:18 +0600

    Inheritance.java

--- First.java
+++ First.java
@@ -33,3 +33,64 @@ public class First {
     }
    
 }
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.first;
+
+/**
+ *
+ * @author MASUD
+ */
+//public class Student {
+    public class Student extends First{
+    int id;
+    String section;
+    
+    void display2(){
+        System.out.println("Child Class");
+        doit();      
+    } 
+    @Override
+    void display1(){
+        super.display1();
+        System.out.println("Id:"+id);
+        System.out.println("Section:"+section);
+    }
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.first;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Mehrab";
+        s1.age = 22;
+        s1.id = 733;
+        s1.section = "4B";
+        s1.weight = 68;
+        
+        s1.display2();
+        System.out.println("--------------");
+        s1.display1();
+        System.out.println("--------------");
+        s1.doit();
+        
+        
+    }
+    }
+    
+
+
+    
+
+
