75dc6e33161d84e476111d5ac31702e5c6b107af
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2023-12-12 21:32:53 +0600

    Add files via upload
    Inheritance

---
+++ First.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.first;
+
+/**
+ *
+ * @author MASUD
+ */
+
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+/**
+ *
+ * @author MASUD
+ */
+public class First {
+    String name;
+    int age;
+    int weight;
+    void display1(){
+        System.out.println("Parent class");
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Weight: "+weight);
+    }
+    void doit(){
+        System.out.println("Doing nothing");   
+    }
+   
+}
