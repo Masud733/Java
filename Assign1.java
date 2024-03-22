f6b1403ff5ffa5281cca2e2079b898e5b1b866a1
Masud Mehrab <141769814+Masud733@users.noreply.github.com>
2024-03-22 16:42:03 +0600

    Add files via upload

---
+++ Assign1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assign1;
+
+/**
+ *
+ * @author MASUD
+ */
+public class Assign1 {
+
+public static void main(String[] args) {
+        String originalString = "JAVA IS MORE FUN THAN C++.";
+        String lowerCaseString = originalString.toLowerCase();
+
+        System.out.println("Original String: " + originalString);
+        System.out.println("Lower Case String: " + lowerCaseString);
+    }
+}
